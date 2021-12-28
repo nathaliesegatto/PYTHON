42e8932fa5c9b52048fb3c7a272fbedb1d506a89
nmorel <nathalie.s.morel@gmail.com>
2021-12-28 18:31:59 -0300

    Cláusula where alterada para for.

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,3 +1,10 @@
+# JOGO DE ADIVINHAÇÃO COM "FOR"
+# O usuário pode inserir diversos valores até que acerte o "número secreto".
+# Quantidade de tentativas LIMITADAS.
+# Função "RANGE", o último valor não é considerado. EX: (1,3) - vai repetir 2 vezes.
+# Função "RANGE", é possível incluir passo. EX: (1,10,2) - vai repetir de 1 a 9 a cada 2 números, ou seja, 4 vezes.
+
+
 print("**********************************")
 print()
 print("Bem-vindo ao jogo de adivinhação!")
@@ -9,29 +16,29 @@ numero_secreto = 17
 tentativas = 3
 rodada = 1
 
-while (rodada <= tentativas):
-    print('Tentativa {} de {}'.format(rodada, tentativas))
-    chute = int(input('Digite um número inteiro de 1 a 20: '))
+for rodada in range(1, tentativas + 1):
+    print('Tentativa {} de {}'.format(rodada,
+                                      tentativas))  # Função "FORMAT" (.format(x,y)) para atribuir valores das variáveis aos colchetes.
+    chute = int(input('Digite um número inteiro entre 1 e 20: '))
     if (chute == numero_secreto):
-      print()
-      print('Parabéns! Você acertou.')
-      print()
-      print('FIM DE JOGO.')
-      rodada = (tentativas + 4)
+        print()
+        print('Parabéns! Você acertou.')
+        print()
+        break  # O COMANDO BREAK ENCERRA O LAÇO DE REPETIÇÃO.
     else:
-      if (chute < numero_secreto):
-        print('O número digitado é menor que o número secreto.')
-        print()
-        rodada = rodada + 1
-      elif (chute > numero_secreto):
-        print('O número digitado é maior que o número secreto.')
-        print()
-        rodada = rodada + 1
+        if (chute < numero_secreto):
+            print('O número digitado é menor que o número secreto.')
+            print()
+            rodada = rodada + 1
+        elif (chute > numero_secreto):
+            print('O número digitado é maior que o número secreto.')
+            print()
+            rodada = rodada + 1
 
 if (rodada == tentativas + 1):
- print ()
- print('Número de tentativas excedido.')
- print('FIM DE JOGO!')
-elif (rodada <= tentativas):
- print()
- print('FIM DE JOGO!')
+    print()
+    print('Número de tentativas excedido.')
+    print('FIM DE JOGO!')
+else:
+    print()
+    print('FIM DE JOGO!')
