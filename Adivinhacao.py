1367d1c9a76d18dd4e1a3354c3b6e3a8c88ada42
nmorel <nathalie.s.morel@gmail.com>
2021-12-28 18:39:48 -0300

    Adicionada função random para gerar núm. aleatório e  linha para mostrar o número secreto.

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,3 +1,13 @@
+# JOGO DE ADIVINHAÇÃO COM - USANDO A FUNÇÃO RANDOM
+# O usuário pode inserir diversos valores até que acerte o "número secreto".
+# Quantidade de tentativas LIMITADAS.
+# Função "RANGE", o último valor não é considerado. EX: (1,3) - vai repetir 2 vezes.
+# Função "RANGE", é possível incluir passo. EX: (1,10,2) - vai repetir de 1 a 9 a cada 2 números, ou seja, 4 vezes.
+# Função "FORMAT" (.format(x,y)) aplicada na linha 18 para atribuir valores das variáveis aos colchetes.
+# A função Random não faz parte do "Built In", é necessário importar (import random)
+# O número "random.random" vem entre 0 e 1 com casas decimais, é necessário multiplicá-lo por 100 e usar o arredondamento (round)
+# https://docs.python.org/3/library/random.html
+
 print("**********************************")
 print()
 print("Bem-vindo ao jogo de adivinhação!")
@@ -5,14 +15,17 @@ print()
 print('Você terá 3 chances para acertar o número secreto.')
 print()
 
-numero_secreto = 17
+import random
+
+numero_secreto = round(
+    random.random() * 100)  # O número "Random" vem entre 0 e 1 com casas decimais, é necessário multiplicá-lo por 100 e usar o arredondamento (round)
 tentativas = 3
 rodada = 1
 
 for rodada in range(1,
                     tentativas + 1):  # O NÚMERO 1 REPRESENTA A PARTIDA, E O ÚLTIMO NÚMERO SERÁ SEMPRE O ANTERIOR AO ESCRITO
     print('Tentativa {} de {}'.format(rodada, tentativas))
-    chute = int(input('Digite um número inteiro entre 1 e 20: '))
+    chute = int(input('Digite um número inteiro entre 1 e 100: '))
     if (chute < 1 or chute > 100):
         print()
         print('O número digitado é inválido.')
@@ -37,6 +50,7 @@ for rodada in range(1,
 if (rodada == tentativas + 1):
     print()
     print('Número de tentativas excedido.')
+    print('A resposta correta é:', numero_secreto, '.')
     print('FIM DE JOGO!')
 else:
     print()
