f7c524d03d41781d1a4a9e111371562becf4397a
nmorel <nathalie.s.morel@gmail.com>
2021-12-28 19:03:05 -0300

    Adicionados níveis de dificuldade com base no intervalo de números.

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -9,49 +9,161 @@
 # https://docs.python.org/3/library/random.html
 
 print("**********************************")
-print()
 print("Bem-vindo ao jogo de adivinhação!")
+print("**********************************")
 print()
-print('Você terá 3 chances para acertar o número secreto.')
+print('Qual o nível de dificuldade desejado? ')
+print('(1) Fácil')
+print('(2) Médio')
+print('(3) Difícil')
 print()
 
-import random
+nivel = int(input('Digite a opção desejada: '))
+print()
 
-numero_secreto = round(
-    random.random() * 100)  # O número "Random" vem entre 0 e 1 com casas decimais, é necessário multiplicá-lo por 100 e usar o arredondamento (round)
-tentativas = 3
-rodada = 1
+while (nivel < 1):
+    print()
+    print('** NÍVEL INVÁLIDO! **')
+    print()
+    print('(1) Fácil')
+    print('(2) Médio')
+    print('(3) Difícil')
+    print()
+    nivel = int(input('Digite a opção desejada: '))
+    print()
 
-for rodada in range(1,
-                    tentativas + 1):  # O NÚMERO 1 REPRESENTA A PARTIDA, E O ÚLTIMO NÚMERO SERÁ SEMPRE O ANTERIOR AO ESCRITO
-    print('Tentativa {} de {}'.format(rodada, tentativas))
-    chute = int(input('Digite um número inteiro entre 1 e 100: '))
-    if (chute < 1 or chute > 100):
-        print()
-        print('O número digitado é inválido.')
-        print()
-        continue  # O COMANDO CONTINUE RETORNA PARA O INÍCIO DO LAÇO DE REPETIÇÃO.
-    else:
-        if (chute == numero_secreto):
+while (nivel > 3):
+    print()
+    print('** NÍVEL INVÁLIDO! **')
+    print()
+    print('(1) Fácil')
+    print('(2) Médio')
+    print('(3) Difícil')
+    print()
+    nivel = int(input('Digite a opção desejada: '))
+    print()
+
+if (nivel == 1):
+    import random
+
+    numero_secreto = random.randrange(1,
+                                      11)  # O número "random.randrange" exige parâmetro (entre 1 e 101 nesse caso, pois considera o número anterior ao digitado)
+    tentativas = 3
+    rodada = 1
+
+    for rodada in range(1,
+                        tentativas + 1):  # O NÚMERO 1 REPRESENTA A PARTIDA, E O ÚLTIMO NÚMERO SERÁ SEMPRE O ANTERIOR AO ESCRITO
+        print('Tentativa {} de {}'.format(rodada, tentativas))
+        chute = int(input('Digite um número inteiro entre 1 e 10: '))
+        if (chute < 1 or chute > 100):
             print()
-            print('Parabéns! Você acertou.')
+            print('O número digitado é inválido.')
             print()
-            break  # O COMANDO BREAK ENCERRA O LAÇO DE REPETIÇÃO.
+            continue  # O COMANDO CONTINUE RETORNA PARA O INÍCIO DO LAÇO DE REPETIÇÃO.
         else:
-            if (chute < numero_secreto):
-                print('O número digitado é menor que o número secreto.')
+            if (chute == numero_secreto):
                 print()
-                rodada = rodada + 1
-            elif (chute > numero_secreto):
-                print('O número digitado é maior que o número secreto.')
+                print('Parabéns! Você acertou.')
                 print()
-                rodada = rodada + 1
+                break  # O COMANDO BREAK ENCERRA O LAÇO DE REPETIÇÃO.
+            else:
+                if (chute < numero_secreto):
+                    print()
+                    print('*** O número secreto é MAIOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
+                elif (chute > numero_secreto):
+                    print()
+                    print('*** O número secreto é MENOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
 
-if (rodada == tentativas + 1):
-    print()
-    print('Número de tentativas excedido.')
-    print('A resposta correta é:', numero_secreto, '.')
-    print('FIM DE JOGO!')
-else:
-    print()
-    print('FIM DE JOGO!')
+    if rodada == tentativas + 1:
+        print()
+        print('Número de tentativas excedido.')
+        print('A resposta correta é:', numero_secreto, '.')
+        print('FIM DE JOGO!')
+    else:
+        print()
+        print('FIM DE JOGO!')
+elif (nivel == 2):
+    import random
+
+    numero_secreto = random.randrange(1,
+                                      51)  # O número "random.randrange" exige parâmetro (entre 1 e 101 nesse caso, pois considera o número anterior ao digitado)
+    tentativas = 3
+    rodada = 1
+
+    for rodada in range(1,
+                        tentativas + 1):  # O NÚMERO 1 REPRESENTA A PARTIDA, E O ÚLTIMO NÚMERO SERÁ SEMPRE O ANTERIOR AO ESCRITO
+        print('Tentativa {} de {}'.format(rodada, tentativas))
+        chute = int(input('Digite um número inteiro entre 1 e 50: '))
+        if (chute < 1 or chute > 100):
+            print()
+            print('O número digitado é inválido.')
+            print()
+            continue  # O COMANDO CONTINUE RETORNA PARA O INÍCIO DO LAÇO DE REPETIÇÃO.
+        else:
+            if (chute == numero_secreto):
+                print()
+                print('Parabéns! Você acertou.')
+                print()
+                break  # O COMANDO BREAK ENCERRA O LAÇO DE REPETIÇÃO.
+            else:
+                if (chute < numero_secreto):
+                    print()
+                    print('*** O número secreto é MAIOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
+                elif (chute > numero_secreto):
+                    print()
+                    print('*** O número secreto é MENOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
+
+    if (rodada == tentativas + 1):
+        print()
+        print('Número de tentativas excedido.')
+        print('A resposta correta é:', numero_secreto, '.')
+        print('FIM DE JOGO!')
+    else:
+        print()
+        print('FIM DE JOGO!')
+elif (nivel == 3):
+    import random
+
+    numero_secreto = random.randrange(1, 101)
+    tentativas = 3
+    rodada = 1
+    for rodada in range(1,
+                        tentativas + 1):  # O NÚMERO 1 REPRESENTA A PARTIDA, E O ÚLTIMO NÚMERO SERÁ SEMPRE O ANTERIOR AO ESCRITO
+        print('Tentativa {} de {}'.format(rodada, tentativas))
+        chute = int(input('Digite um número inteiro entre 1 e 100: '))
+        if (chute < 1 or chute > 100):
+            print()
+            print('O número digitado é inválido.')
+            print()
+            continue  # O COMANDO CONTINUE RETORNA PARA O INÍCIO DO LAÇO DE REPETIÇÃO.
+        else:
+            if (chute == numero_secreto):
+                print()
+                print('Parabéns! Você acertou.')
+                print()
+                break  # O COMANDO BREAK ENCERRA O LAÇO DE REPETIÇÃO.
+            else:
+                if (chute < numero_secreto):
+                    print('*** O número secreto é MAIOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
+                elif (chute > numero_secreto):
+                    print('*** O número secreto é MENOR que', chute, ' ***')
+                    print()
+                    rodada = rodada + 1
+    if (rodada == tentativas + 1):
+        print()
+        print('Número de tentativas excedido.')
+        print('A resposta correta é:', numero_secreto, '.')
+        print('FIM DE JOGO!')
+    else:
+        print()
+        print('FIM DE JOGO!')
