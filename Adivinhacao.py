c5c5e1b0015e34171ed4e7242a5471ba4c874b4d
nmorel <nathalie.s.morel@gmail.com>
2021-12-28 18:28:51 -0300

    Adicionado 3 tentativas para acertar o número com cláusula where.

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,27 +1,37 @@
-print ("**********************************")
+print("**********************************")
 print()
-print ("Bem-vindo ao jogo de adivinhação!")
+print("Bem-vindo ao jogo de adivinhação!")
 print()
-print ("**********************************")
-
-numero_secreto = 7
-chute = int (input("Digite um número inteiro: "))
+print('Você terá 3 chances para acertar o número secreto.')
 print()
 
-while (chute != numero_secreto):
-  if (chute > numero_secreto):
-    print()
-    print ('O número digitado é maior que o número secreto!')
-    print()
-    chute = int (input('Digite outro número inteiro: '))
-  else:
-    print ()
-    print ('O número digitado é menor que o número secreto!')
-    print ()
-    chute = int (input('Digite outro número inteiro: '))
+numero_secreto = 17
+tentativas = 3
+rodada = 1
 
-print()
-print ('***** Muito bem, você acertou!*****')
+while (rodada <= tentativas):
+    print('Tentativa {} de {}'.format(rodada, tentativas))
+    chute = int(input('Digite um número inteiro de 1 a 20: '))
+    if (chute == numero_secreto):
+      print()
+      print('Parabéns! Você acertou.')
+      print()
+      print('FIM DE JOGO.')
+      rodada = (tentativas + 4)
+    else:
+      if (chute < numero_secreto):
+        print('O número digitado é menor que o número secreto.')
+        print()
+        rodada = rodada + 1
+      elif (chute > numero_secreto):
+        print('O número digitado é maior que o número secreto.')
+        print()
+        rodada = rodada + 1
 
-print ()
-print ('FIM DO JOGO.')
+if (rodada == tentativas + 1):
+ print ()
+ print('Número de tentativas excedido.')
+ print('FIM DE JOGO!')
+elif (rodada <= tentativas):
+ print()
+ print('FIM DE JOGO!')
